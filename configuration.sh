bad8f248a29d964c75d1fbcff1d7400b8aa595e5
Zain Bin Imran <l1f21bsse0539@ucp.edu.pk>
2025-01-20 09:02:30 +0500

    Iitial Commit

---
+++ configuration.sh
@@ -0,0 +1,8 @@
+echo "Repo Initial Configuration Started"
+git branch -m "develop"
+git config --local user.name "Zain Bin Imran"
+git config --local user.email "l1f21bsse0539@ucp.edu.pk"
+gti config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
+npm init -y 
